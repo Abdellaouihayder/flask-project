32e54ce577582e0d2d329b0d5cdba36d078fa7e9
hayder <haidaran2002@gmail.com>
2025-02-01 23:10:36 +0100

    CRUD

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,request,jsonify
+from flask_cors import cross_origin
 import products
 
 pobj=products.Products()
@@ -14,10 +15,82 @@ def index():
 def about():
     return "About Page"
 
-@app.route('/products')
+@app.route('/products',methods=['GET','POST'])
+@cross_origin(origins="*",headers=['content-type'])
 def productList():
-    data=pobj.getProducts()
-    return jsonify(data)
+    if(request.method=='GET'):
+        data=pobj.getProducts()
+        if len(data):
+            response=jsonify({
+                "resultat":data,
+                "status":200
+                })
+        else:
+            response=jsonify({
+                "resultat":data,
+                "status":400
+                })
+        return response
+    elif(request.method=='POST'):
+        data=request.json
+        aData=pobj.createProduct(data)
+        if(aData==200):
+            response=jsonify({
+                "resultat":aData,
+                "status":200,
+                "message":"created successfully"
+            })
+        else:
+            response=jsonify({
+                "resultat":aData,
+                "status":400,
+                "message":"failed of creation"})
+                
+        return response
+#update data or delete using this url
+@app.route('/product/<int:pid>',methods=['GET','PUT','DELETE','PATCH','OPTIONS'])
+@cross_origin(origins="*",headers=['content-type'])
+def productchange(pid):
+    if(request.method=='PUT'):
+        data=request.json
+        aData=pobj.updateProduct(data,pid)
+        if(aData==200):
+            reponse=jsonify({
+                "result":aData,
+                "status":200,
+                "message":"updated Successfully"
+            })
+        else:
+            reponse=jsonify({
+                "result":aData,
+                "status":400,
+                "message":"Failed to update"
+            })
+        return reponse
+    elif(request.method=='DELETE'):
+        aData=pobj.deleteProduct(pid)
+        if(aData==200):
+            reponse=jsonify({
+            "result":aData,
+            "status":200,
+            "message":"Delete Successfully"
+            })
+        else:
+            reponse=jsonify({
+            "result":aData,
+            "status":400,
+            "message":"Failed to delete"
+            })
+        return reponse
+
+    elif(request.method=='PATCH'):
+        return "Patch http method"
+    elif(request.method=='GET'):
+        return "request to get single product"
+    else:
+        return("options http method")
+    return reponse
+
 
 if __name__ == "__main__":
-    app.run(debug=True)  # Make sure debug mode is enabled
+    app.run(debug=True)  
--- products.py
+++ products.py
@@ -13,7 +13,56 @@ class Products:
                 'title':result[1],
                 'price':result[2],
                 'Description':result[3],
+                'review':result[4],
+                'rating':result[5],
+                'link':result[6],
+
             }
             payload.append(content)
             content={}
         return payload
+    def createProduct(self,data):
+        title=data['title']
+        price=data['price']
+        Description=data['Description']
+        review=data['review']
+        rating=data['rating']
+        link=data['link']
+        if(data['id']==''):
+            sql="INSERT INTO `product`(`title`, `price`, `description`, `review`, `rating`, `link`) VALUES (%s,%s,%s,%s,%s,%s)"
+            rez=self.cursor.execute(sql,(title,price,Description,review,rating,link))
+            self.cursor.connection.commit()
+        else:
+            id=data['id']
+            sql="INSERT INTO `product`(`id`,`title`, `price`, `description`, `review`, `rating`, `link`) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+            rez=self.cursor.execute(sql,(id,title,price,Description,review,rating,link))
+            self.cursor.connection.commit()
+
+        if(rez==1):
+            return 200
+        else:
+            return 400
+    def updateProduct(self,data,pid):
+        #id=data['id']
+        title=data['title']
+        price=data['price']
+        Description=data['Description']
+        review=data['review']
+        rating=data['rating']
+        link=data['link']
+        sql="UPDATE `product` SET `title`=%s, `price`=%s, `description`=%s, `review`=%s, `rating`=%s, `link`=%s WHERE `id`=%s"
+        rez=self.cursor.execute(sql,(title,price,Description,review,rating,link,pid))
+        self.cursor.connection.commit()
+
+        if(rez==1):
+            return 200
+        else:
+            return 400
+    def deleteProduct(self,pid):
+        sql="DELETE FROM `product` WHERE id=%s"
+        rez=self.cursor.execute(sql,pid)
+        self.cursor.connection.commit()
+        if(rez==1):
+            return 200
+        else:
+            return 400
